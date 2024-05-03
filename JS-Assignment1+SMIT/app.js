ae400ac0dacd0c6f9e52e8f9702bc43b98be234f
HafizMudassirHusain <humdassir511@gmail.com>
2024-05-03 12:46:52 -0700

    first assignmets

---
+++ JS-Assignment1+SMIT/app.js
@@ -0,0 +1,307 @@
+// var studentName = prompt("Enter your name: ");
+// var studentId = prompt("enter your ID: ");
+// var DBMS = +prompt("Enetr your DBMS Marks: ");
+// var autometa = +prompt("Enetr your Autometa Marks: ");
+// var linearalgeb = +prompt("Enetr your LinearAlgebra Marks: ");
+// var Design = +prompt("Enetr your Design Analysis Marks: ");
+// var philosophy = +prompt("Enetr your Philosophy Marks: ");
+// var obtainMarks = DBMS + autometa + linearalgeb + Design + philosophy;
+// var totalMarks = 500;
+// var Percentage =  (obtainMarks / totalMarks) *100 ;
+// var grades;
+// if(Percentage  >=80){
+//   grades ="A";
+//   }else if(Percentage >= 70){
+//     grades =  "B";
+//   }
+//   else if(Percentage >= 60){
+//     grades =  "C";
+//   }
+//   else if(Percentage >= 50){
+//     grades =  "D";
+//   }
+//   else{
+//     grades = "F";
+//   }
+// document.write(`<table border=1px><tr><th>StudentName</th><th>Student ID</th><th>DataBase</th><th>Autometa</th><th>LinearAlgeb</th><th>Design</th><th>Philosophy</th><th>Obtain Marks</th><th>Total Marks</th><th>Grades</th><th>Pencentage</th></tr>
+// <tr>
+//     <td>${studentName}</td>
+//     <td>${studentId}</td>
+//     <td>${DBMS}</td>
+//     <td>${autometa}</td>
+//     <td>${linearalgeb}</td>
+//     <td>${Design} </td>
+//     <td>${philosophy}</td>
+//     <td>${obtainMarks}</td>
+//     <td>${totalMarks}</td>
+//     <td>${grades}</td>
+//     <td>${Percentage}</td>
+// </tr> </table>`);
+ 
+
+
+
+
+
+
+    // ++++++++++++++++++++++++++++++++++++++++ASIGNMENT NO 01++++++++++++++++++++++++++++++++++++++++
+// ==============================Aleart 1==============================
+// alert("welcome to our website")
+
+// ==============================Question 2========================
+// alert("Error! Please enter valid Password: ");
+
+// ==============================Question 3========================
+// alert("welcome to JS landing Page.. \n Happy coding")
+
+// ==============================Question 4========================
+// alert("welcome to JS landing")
+// alert(" Happy coding!")
+
+// ==============================Question 5========================
+// var alrt = "Hello! i can run js through my js web browser's console" 
+// alert(alrt)
+// console.log(alrt)
+
+// ==============================Alert secion is completed========================
+
+// ==============================VARIABLE========================
+// ==============================Question 1========================
+// var userName
+
+// ==============================Question 2========================
+// var myNAme = "Hafiz Mudassir Husain"
+
+// ==============================Question 3========================
+// var message = "Hello World"
+// alert(message)
+
+// ==============================Question 4========================
+// var myName ="ALi Khan"
+// var age = "17 years old"
+// var university = "SMIU"
+// alert(myName)
+// alert(age)
+// alert(university)
+
+// ==============================Question 5========================
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+// ==============================Question 6========================
+// var numOfChild = 3
+// var futurePartner  = "Na maloom "
+// var geolocation = "by default Pakistan"
+// var job =  " full stack Developer"
+
+// alert(`i will be a ${job} in ${geolocation} and married to ${futurePartner} with ${numOfChild} kids`)
+
+// ==============================Question 7========================
+// var email = "hmudassir@gmail.com"
+// alert("my email adress is : " + email)
+
+// ==============================Question 8========================
+// var book = "A Smater Way to Learn JavaScript"
+// alert("i am trying tolearn from the book " + book)
+
+// ==============================Question 9========================
+// var myName = "My name is mudassir husain "
+// var age = "I am 22 year old "
+// var uniName = "I study in SMIU "
+// var major = "Computer Science"
+// alert (`${myName}\n${age}\n${uniName}\nMajor: ${major} `)
+
+// ============================= Question 10==========================
+// document.write("Yay! I can write HTML content through JavaScript")
+
+// ====================================Variable section completed=============================
+
+// =============================VARIABLE NUMBAR ====================================
+// =================================Question NO 1===================================
+// var age ="i am 22 years old"
+// alert(age)
+
+// =================================Question NO 2===================================
+// alert("you have visited this site 21 times")
+
+// =================================Question NO 3===================================
+// var birthYear = "I was born in 2005"
+// document.write(birthYear)
+// document.write("<br>")
+// document.write("Data type of my declered variable is "+ typeof(birthYear))
+
+// =================================Question NO 3===================================
+// var nameOfVisitor = prompt("what's your name: ");
+// var productTittle = prompt("which product you want buy these store")
+// var storeName = "Amazon online shopping services"
+// document.write(`${nameOfVisitor} ordered 3 ${productTittle} on ${storeName}`)
+
+// =================================COmpleted NOw===================================
+
+// ========================4 Variable NAme ============================================
+// ============================Question No 1===============================================
+// var x = 1, y = 2, z = 3;
+
+// ============================Question No 2===============================================
+// =legals variables name in js===========
+// var myVariable;
+// var $name = "mudassir";
+// var _age;
+// var camelCaseVariable;
+// var another123Variable;
+
+// 2. Declare 5 illegal variable names
+  // var 123illegal; // Cannot start with a number
+  // var my-variable; // Hyphens are not allowed
+  // var my variable; // Space is not allowed
+  // var my@variable; // Special characters except $ and _ are not allowed
+  // var if; // Reserved keyword
+
+// ============================Question No 3===============================================
+// Display variable naming rules
+// document.write("A heading stating “Rules for naming JS variables"+ "<br>");
+// document.write("Variable names can only contain letters, numbers, $, and _."+ "<br>")
+// document.write("Variables must begin with a letter, $, or _. For example $name, _name or name"+ "<br>");
+// document.write("Variable names are case-sensitive."+ "<br>");
+// document.write("Variable names should not be JS reserved keywords.");
+
+// =================================Completed========================
+
+// ========================================MAths Expression============================
+// ============================Question No 1===============================================
+// var num1 = +prompt("enter first number: ")
+// var num2 = +prompt("enter second number: ")
+// var result = num1 + num2 
+// document.write( "sum of "+ num1+" and "+num2 + "is " + result)
+
+// ============================Question No 2===============================================
+// var num1 = +prompt("enter first number: ")
+// var num2 = +prompt("enter second number: ")
+// var sum = num1 + num2
+// var sub = num1 - num2
+// var mul = num1 * num2
+// var div = num1 / num2
+// document.write( "sum of "+ num1+" and "+num2 + " is " + sum + "<br>")
+// document.write( "subtraction of "+ num1+" and "+num2 + " is " + sub + "<br>")
+// document.write( "multiplication of "+ num1+" and "+num2 + " is " + mul + "<br>")
+// document.write( "divide of "+ num1+" and "+num2 + " is " + div)
+
+// ==================================Question No 3==========================
+//   var myVariable;
+//   document.write("Value after variable declaration is: " + myVariable + "<br>");
+//   myVariable = 3;
+//   document.write("Initial value: " + myVariable + "<br>");
+//   myVariable++;
+//   document.write("Value after increment is: " + myVariable + "<br>");
+//   myVariable += 7;
+//   document.write("Value after addition is: " + myVariable + "<br>");
+//   myVariable--;
+//   document.write("Value after decrement is: " + myVariable + "<br>");
+//   var remainder = myVariable % 3;
+//   document.write("The remainder is: " + remainder);
+
+// =======================Question no 4=================================================
+// var ticketPrice = 600;
+// var numberOfTickets = 7;
+// var totalCost = ticketPrice * numberOfTickets;
+// document.write("The total cost of buy " + numberOfTickets + " movie tickets is: " + totalCost + " PKR");
+
+// =======================Question no 5=================================================
+//   var number = prompt("Enter a number you want print the table:");
+//   number = parseInt(number);
+//   document.write("Multiplication Table of " + number+ "<br>");
+//   for (var i = 1; i <= 10; i++) {
+//     document.write(number + " x " + i + " = " + (number * i) + "<br>");
+//   }
+
+// =======================Question no 6=================================================
+// var celsiusTemperature = 25;
+// var fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+// document.write(celsiusTemperature + "*C is " + fahrenheitTemperature + "*F<br>");
+// var fahrenheitTemp = 77;
+// var celsiusTemp = (fahrenheitTemp - 32) * 5/9;
+// document.write(fahrenheitTemp + "*F is " + celsiusTemp + "*C");
+
+// =======================Question no 7=================================================
+// var priceOfItem1 = 500;
+// var priceOfItem2 = 700;
+// var quantityItem1 = 2;
+// var quantityItem2 = 1;
+// var totalItem1 = priceOfItem1 * quantityItem1;
+// var totalItem2 = priceOfItem2 * quantityItem2;
+// var totalCost = totalItem1 + totalItem2;
+// var shippingCharges = 150;
+// var totalCostWithShipping = totalCost + shippingCharges;
+// document.write("Shopping Cart Checkout" +"<br>");
+// document.write("Price of Item 1: " + priceOfItem1 + " PKR <br>");
+// document.write("Quantity of Item 1: " + quantityItem1 + "<br>");
+// document.write("total for Item 1: " + totalItem1 + " PKR <br>");
+
+// document.write("Price of Item 2: " + priceOfItem2 + " PKR<br>");
+// document.write("Quantity of Item 2: " + quantityItem2 + "<br>");
+// document.write("Subtotal for Item 2: " + totalItem2 + " PKR<br><br>");
+
+// document.write("Total cost of all items: " + totalCost + " PKR<br>");
+// document.write("Shipping charges: " + shippingCharges + " PKR<br>");
+// document.write("Total cost including shipping charges: " + totalCostWithShipping + " PKR");
+
+// =======================Question no 8=================================================
+// var totalMarks = prompt("Enter total marks:");
+// var marksObtained = prompt("Enter marks obtained:");
+// totalMarks = parseInt(totalMarks);
+// marksObtained = parseInt(marksObtained);
+// var percentage = (marksObtained / totalMarks) * 100;
+// document.write("Mark-Sheet Calculater <br>");
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Marks Obtained: " + marksObtained + "<br>");
+// document.write("Percentage: " + percentage.toFixed(2) + "%");
+
+
+// =======================Question no 9=================================================
+// var usdAmount = 10;
+// var sarAmount = 25;
+// var usdToPkr = 244;
+// var sarToPkr = 28;
+// var totalPkr = (usdAmount * usdToPkr) + (sarAmount * sarToPkr);
+// document.write("Currency Conversion <br>");
+// document.write("Amount in US Dollars: " + usdAmount + " USD<br>");
+// document.write("Amount in Saudi Riyals: " + sarAmount + " SAR<br>");
+// document.write("Total Amount in Pakistani Rupees: " + totalPkr + " PKR");
+
+// =======================Question no 10=================================================
+  // var number = +prompt( 'Please enter a positive integer: ' )
+  // var result = ((number + 5) * 10) / 2
+  // alert("Initial Number: " + number + "\n" + "Result after sequence of operations: " + result)
+
+
+// =======================Question no 11=================================================
+// var currentYear = +prompt( "Enter the current year: " );
+// var birthYear = +prompt("Enter your birth year: "); 
+// var age1 = currentYear - birthYear;
+// var age2 = age1 - 1;
+// document.write("Age Calculator <br>");
+// document.write("current year: "+ currentYear);
+// document.write("<br> Birth Year: " + birthYear);
+// document.write("<br> They are either " + age1 + " or " + age2 + " years old.");
+
+// =======================Question no 12=================================================
+  // var radius = 12;
+  // var pi = 3.142;
+  // var circumference = 2 * pi * radius;
+  // document.write("Circle Properties Calculator <br>");
+  // document.write("circumference is " + circumference.toFixed(2) + ".<br>");
+  // var area = pi * radius * radius;
+  // document.write("area is " + area.toFixed(2) + ".");
+
+// =======================Question no 13=================================================
+// var favoriteSnack = "chocolate bars";
+// var currentAge = 30;
+// var maxAge = 80;
+// var amountPerDay = 2; 
+// var remainingYears = maxAge - currentAge;
+// var totalAmount = amountPerDay * 365 * remainingYears;
+// document.write("<h2>Lifetime Supply Calculator</h2>");
+// document.write("Favorite Snack: " + favoriteSnack + "<br>");
+// document.write("Current Age: " + currentAge + "<br>");
+// document.write("Maximum Age: " + maxAge + "<br>");
+// document.write("Estimated Amount Per Day: " + amountPerDay + "<br>");
+// document.write("You will need " + totalAmount + " " + favoriteSnack + " to last you until the ripe old age of " + maxAge + ".");
